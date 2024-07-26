6f9c78942ceb7e94393854b663d9d00ec0dc6897
Deepak Chauhan <imdeepakchauhan12@gmail.com>
2024-07-26 18:48:02 +0530

    create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 
 // import TypeORM
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 /**
  * User created modules
@@ -18,15 +19,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     PostsModule,
     AuthModule,
     // Connection to DB
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      entities: [],
-      synchronize: true,
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      host: 'localhost',
-      database: 'nestjs-blog',
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [User],
+        synchronize: true,
+        port: 5432,
+        username: 'postgres',
+        password: 'password',
+        host: 'localhost',
+        database: 'nestjs-blog',
+      }),
     }),
   ],
   controllers: [AppController],
--- src/users/dtos/create-user-dto.ts
+++ src/users/dtos/create-user-dto.ts
@@ -24,11 +24,13 @@ export class CreateUserDTO {
   @IsString()
   @IsNotEmpty()
   @IsEmail()
+  @MaxLength(96)
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
+  @MaxLength(96)
   @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
     message:
       'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number',
